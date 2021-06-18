f88eaaead20db25adb7df38c564db215ef142d3c
pneismeis <34748929+pneismeis@users.noreply.github.com>
2021-06-18 13:49:34 +0200

    Update wsl2_install_part1.ps1

--- wsl2_install_part1.ps1
+++ wsl2_install_part1.ps1
@@ -60,3 +60,82 @@ function Get-WSLExistance ($distro) {
     }
     return $installed
 }
+
+function Get-StoreDownloadLink ($distro) {
+    # Uses $distro.StoreLink to get $distro.URI
+    # Required when URI is not hard-coded
+    #### Thanks to MattiasC85 for this excelent method of getting Microsoft Store download URIs ####
+    # Source: https://github.com/MattiasC85/Scripts/blob/a1163b97875ed075927438505808622614a9961f/OSD/Download-AppxFromStore.ps1
+    $wchttp=[System.Net.WebClient]::new()
+    $URI = "https://store.rg-adguard.net/api/GetFiles"
+    $myParameters = "type=url&url=$($distro.StoreLink)"
+    $wchttp.Headers[[System.Net.HttpRequestHeader]::ContentType]="application/x-www-form-urlencoded"
+    $HtmlResult = $wchttp.UploadString($URI, $myParameters)
+    $Start=$HtmlResult.IndexOf("<p>The links were successfully received from the Microsoft Store server.</p>")
+    if ($Start -eq -1) {
+        write-host "Could not get Microsoft Store download URI, please check the StoreURL."
+        exit
+    }
+    $TableEnd=($HtmlResult.LastIndexOf("</table>")+8)
+    $SemiCleaned=$HtmlResult.Substring($start,$TableEnd-$start)
+    $newHtml=New-Object -ComObject "HTMLFile"
+    $src = [System.Text.Encoding]::Unicode.GetBytes($SemiCleaned)
+    $newHtml.write($src)
+    $ToDownload=$newHtml.getElementsByTagName("a") | Select-Object textContent, href
+    $apxLinks = @()
+    $ToDownload | Foreach-Object {
+        if ($_.textContent -match '.appxbundle') {
+            $apxLinks = $_
+        }
+    }
+    $distro.URI = $apxLinks.href
+    return $distro
+}
+
+function Check-Sideload (){
+    # Return $true if sideloading is enabled
+    $keyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+    $Key = Get-Item -LiteralPath $keyPath
+    $sideloadKeys = @("AllowAllTrustedApps", "AllowDevelopmentWithoutDevLicense")
+    $return = $true
+    function Test-RegProperty ($propertyname){
+        if (($Key.GetValue($propertyname, $null)) -ne $null){
+            return $true
+        } else {
+            return $false
+        }
+    }
+    $sideloadKeys | ForEach-Object {
+        if (!(Test-RegProperty ($_))){
+            $return = $false
+        } else {
+            if (( (Get-ItemProperty -Path $keyPath -Name $_).$_ ) -ne 1 ){
+                $return = $false
+            }
+        }
+    }
+    return $return
+}
+function Enable-Sideload () {
+    # Allow sideloading of unsigned appx packages
+    $keyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+    if (!(Test-Path -Path $keyPath)){
+        New-Item -Path $keyPath # In case the entire registry key was accidentally deleted
+    }
+    $Key = Get-Item -LiteralPath $keyPath
+    $sideloadKeys = @("AllowAllTrustedApps", "AllowDevelopmentWithoutDevLicense")
+    function Test-RegProperty ($propertyname){
+        if (($Key.GetValue($propertyname, $null)) -ne $null){
+            return $true
+        } else {
+            return $false
+        }
+    }
+    $sideloadKeys | ForEach-Object {
+        if (!(Test-RegProperty $_)){
+            New-ItemProperty -Path $keyPath -Name $_ -Value "1" -PropertyType DWORD -Force | Out-Null
+        } else {
+            Set-ItemProperty -Path $keyPath -Name $_ -Value "1" -PropertyType DWORD -Force | Out-Null
+        }
+    }
+}
