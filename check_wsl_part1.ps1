88c9b458bfc0f56668f8e15557ea5d34b98fa80e
pneismeis <34748929+pneismeis@users.noreply.github.com>
2021-06-22 13:51:12 +0200

    Create check_wsl_part1.ps1

---
+++ check_wsl_part1.ps1
@@ -0,0 +1,19 @@
+function Get-Kernel-Updated () {
+    # Check for Kernel Update Package
+    Write-Host("Checking for Windows Subsystem for Linux Update...")
+    $uninstall64 = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | ForEach-Object { Get-ItemProperty $_.PSPath } | Select-Object DisplayName, Publisher, DisplayVersion, InstallDate
+    if ($uninstall64.DisplayName -contains 'Windows Subsystem for Linux Update') {
+        return $true 
+    } else {
+        return $false
+    }
+}
+
+
+    if (!(Get-Kernel-Updated)) {
+        Write-Host(" ...WSL kernel update not installed.")
+        Update-Kernel
+    } else {
+        Write-Host(" ...WSL update already installed.")
+    }
+    
